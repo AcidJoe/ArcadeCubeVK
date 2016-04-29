b48dad4fecd4f9ea6e9fdd13e4b4004e3a9a0889
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-29 21:22:24 +0500

    Setup AI

---
+++ Assets/Games/Pong/Scripts/PongAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongAI : MonoBehaviour
+{
+    public GameObject Ball;
+    private Transform CurrentTransform;
+    public int speed;
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        CurrentTransform = transform;
+    }
+
+    void Update()
+    {
+        if (!Ball)
+        {
+            Ball = GameObject.FindGameObjectWithTag("Ball");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CurrentTransform = transform;
+        if (Ball)
+        {
+            if (CurrentTransform.position.x < Ball.transform.position.x)
+            {
+                if (CurrentTransform.position.y < Ball.transform.position.y)
+                {
+                    rb.velocity = new Vector2(0, 1) * speed;
+                }
+                else if (CurrentTransform.position.y > Ball.transform.position.y)
+                {
+                    rb.velocity = new Vector2(0, -1) * speed;
+                }
+                else {
+                    rb.velocity = new Vector2(0, 0) * speed;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Games/Pong/Scripts/PongBall.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongBall : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * speed;
+    }
+
+    void Update()
+    {
+
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        float f = (ballPos.y - racketPos.y) / racketWidth; ;
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return f;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the Racket?
+        if (col.gameObject.tag == "Player")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.y);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            rb.velocity = dir * speed;
+        }
+
+        if (col.gameObject.tag == "AI")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.y);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            rb.velocity = dir * speed;
+        }
+    }
+}
