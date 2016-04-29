c2b6b1e4fc189fe78a7edf167b4180b710ef52af
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-29 11:43:52 +0500

    Add ball

---
+++ Assets/Games/Arkanoid/Scripts/ArkanoidBall.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArkanoidBall : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public bool isActivate;
+
+    public float speed;
+
+    public GameObject player;
+
+    public Vector3 pos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pos = transform.position;
+
+        isActivate = false;
+    }
+
+    void Update()
+    {
+        if (!isActivate && player != null)
+        {
+            pos.x = player.transform.position.x;
+            pos.y = player.transform.position.y + 13.0f;
+
+            transform.position = pos;
+        }
+
+        if (Input.GetButtonDown("Jump") && !isActivate)
+        {
+            isActivate = true;
+            rb.velocity = Vector2.up * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        float f = (ballPos.x - racketPos.x) / racketWidth; ;
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return f;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the Racket?
+        if (col.gameObject.tag == "Player")
+        {
+            // Calculate hit Factor
+            float x = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+}
