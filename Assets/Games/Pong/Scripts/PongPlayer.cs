171253639b202753f495ba268087586777512611
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-29 20:33:44 +0500

    Setup player

---
+++ Assets/Games/Pong/Scripts/PongPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongPlayer : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float dir = Input.GetAxis("Vertical");
+
+        rb.velocity = (new Vector2(0, dir)) * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Border")
+        {
+            rb.velocity = (new Vector3(0, 0, 0) * Time.deltaTime);
+        }
+    }
+}
