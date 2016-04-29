44353f3d6e85329c9f5fbc1738a56dbbdbca0917
Evgeny Novikov <acidjoe1986@gmail.com>
2016-04-29 11:12:06 +0500

    Add Bat script

---
+++ Assets/Games/Arkanoid/Scripts/Bat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bat : MonoBehaviour
+{
+    public float playerVelocity;
+
+    float h;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+
+        rb.velocity = (new Vector3(h, 0, 0) * Time.deltaTime) * playerVelocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Border")
+        {
+            rb.velocity = (new Vector3(0, 0, 0) * Time.deltaTime) * playerVelocity;
+        }
+    }
+}
