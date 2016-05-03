41186854f4ef5a9b70e143c8769eda495851c6fa
Evgeny Novikov <acidjoe1986@gmail.com>
2016-05-03 19:07:25 +0500

    Прикрутил старые скрипты. Нужно настраивать сетку и прочее...

---
+++ Assets/Games/Tetris/Scripts/Group.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour
+{
+    float lastFall = 0;
+
+    void Start()
+    {
+        // Default position not valid? Then it's game over
+        if (!isValidGridPos())
+        {
+            //ToDo GameOver
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        // Move Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(1, 0, 0);
+        }
+
+        // Move Right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        // Rotate
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, -90);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+
+         //Move Downwards and Fall
+        else if (Input.GetKey(KeyCode.DownArrow) && Time.time - lastFall >= 0.3f)
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (isValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+            }
+            else
+            {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.deleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Spawner_Tetris>().spawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+
+            lastFall = Time.time;
+        }
+
+        else if (Time.time - lastFall >= 1)
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (isValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+            }
+            else {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.deleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Spawner_Tetris>().spawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+
+            lastFall = Time.time;
+        }
+    }
+
+    bool isValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+
+            // Not inside Border?
+            if (!Grid.insideBorder(v))
+                return false;
+
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void updateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.h; ++y)
+            for (int x = 0; x < Grid.w; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+}
---
+++ Assets/Games/Tetris/Scripts/Spawner_Tetris.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner_Tetris : MonoBehaviour
+{
+    public GameObject[] shapes;
+
+    void Start ()
+    {
+        spawnNext();
+    }
+
+    public void spawnNext()
+    {
+        // Random Index
+        int i = Random.Range(0, shapes.Length);
+
+        // Spawn Group at current Position
+        Instantiate(shapes[i],
+                    transform.position,
+                    Quaternion.identity);
+    }
+}
