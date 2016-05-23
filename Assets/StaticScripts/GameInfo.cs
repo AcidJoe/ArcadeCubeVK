0f2b0aa0b10d7a688fa5bafe799784b7f46866d9
Evgeny Novikov <acidjoe1986@gmail.com>
2016-05-23 20:17:47 +0500

    Исправил номер версии

--- Assets/StaticScripts/GameInfo.cs
+++ Assets/StaticScripts/GameInfo.cs
@@ -4,5 +4,5 @@ using System.Collections;
 public static class GameInfo
 {
     public static int difficulty;
-    public static string currentVersion = "v.0.1.0";
+    public static string currentVersion = "v.0.1.1";
 }
