da96992ac5b4f285fea83aa151903742917b180a
Evgeny Novikov <acidjoe1986@gmail.com>
2016-05-25 11:09:48 +0500

    Добавил новые забавные картинки в тест

---
+++ Assets/Menues/TestGameScene/TestManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject screen;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckButton();
+        }
+
+        if (screen.activeInHierarchy)
+        {
+            Time.timeScale *= 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    void CheckButton()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            if (hit.collider.gameObject.tag == "Ball")
+            {
+                if (screen.activeInHierarchy)
+                {
+                    screen.SetActive(false);
+                }
+                else
+                {
+                    screen.SetActive(true);
+                }
+            }
+    }
+}
