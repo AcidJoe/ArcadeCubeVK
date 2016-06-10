e1e0efcc1f7eb5cedce9619b21aeb4fb3249ee9e
Evgeny Novikov <acidjoe1986@gmail.com>
2016-06-10 20:53:27 +0500

    Добавил возможность входа с тестовым профилем

--- Assets/Menues/TestMainMenu/Scripts/TestMainMenu.cs
+++ Assets/Menues/TestMainMenu/Scripts/TestMainMenu.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TestMainMenu : MonoBehaviour
 {
+    public Text _name;
+    public Text lvl;
+    public Text exp;
 
-	void Start ()
+    public Text btoken;
+    public Text stoken;
+    public Text gtoken;
+
+    public Image photo;
+
+    void Start ()
     {
-	
+        _name.text = Game.player.name;
 	}
 	
 	void Update ()
     {
-	
-	}
+        lvl.text = "Уровень " + Game.player.lvl.ToString();
+        exp.text = ExpManager.Calculate(Game.player.exp).ToString() +"/"+ExpManager.expToNext;
+        btoken.text = Game.player.b_tokens.ToString();
+        stoken.text = Game.player.s_tokens.ToString();
+        gtoken.text = Game.player.g_tokens.ToString();
+    }
 }
