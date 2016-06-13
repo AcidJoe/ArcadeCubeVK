aa3c06cc6d999c4635372a0832df8ff2eb366802
Evgeny Novikov <acidjoe1986@gmail.com>
2016-06-13 17:11:51 +0500

    Добавил немного функционала. Но случился креш и не знаю что созранилось в итоге...

--- Assets/Menues/TestMainMenu/Scripts/TestMainMenu.cs
+++ Assets/Menues/TestMainMenu/Scripts/TestMainMenu.cs
@@ -18,9 +18,28 @@ public class TestMainMenu : MonoBehaviour
 
     public Sprite sprite;
 
+    public GameObject mainPanel;
+    public GameObject playPanel;
+
+    public GameObject currentPanel;
+    public GameObject backButton;
+
+    public GameObject currentToken;
+
+    public GameObject bronze;
+    public GameObject silver;
+    public GameObject gold;
+
+    public GameObject tokenbutton_b, tokenbutton_s, tokenbutton_g;
+
     void Start()
     {
-        if(Game.player == null)
+        Tokens(false);
+        currentPanel = mainPanel;
+        backButton.SetActive(false);
+        playPanel.SetActive(false);
+
+        if (Game.player == null)
         {
             Game.player = new Profile("Тестер");
         }
@@ -37,5 +56,66 @@ public class TestMainMenu : MonoBehaviour
         gtoken.text = Game.player.g_tokens.ToString();
         progressBar.fillAmount = (float)ExpManager.Calculate(Game.player.exp) / (float)ExpManager.expToNext;
         photo.sprite = sprite;
+
+        if(currentToken && Input.GetMouseButtonUp(0))
+        {
+            Destroy(currentToken);
+        }
+    }
+
+    public void ChangePanels(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                if (!mainPanel.activeInHierarchy)
+                {
+                    currentPanel.SetActive(false);
+                    mainPanel.SetActive(true);
+                    currentPanel = mainPanel;
+                    backButton.SetActive(false);
+                    Tokens(false);
+                }
+                break;
+            case 1:
+                if (!playPanel.activeInHierarchy)
+                {
+                    currentPanel.SetActive(false);
+                    playPanel.SetActive(true);
+                    currentPanel = playPanel;
+                    backButton.SetActive(true);
+                    Tokens(true);
+                }
+                break;
+        }
+    }
+
+    public void takeToken(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                if (!currentToken && Input.GetMouseButton(0))
+                {
+                    currentToken = Instantiate(bronze);
+                }
+                break;
+        }
+    }
+
+    void Tokens(bool activate)
+    {
+        if (activate)
+        {
+            tokenbutton_b.SetActive(true);
+            tokenbutton_s.SetActive(true);
+            tokenbutton_g.SetActive(true);
+        }
+        else if (!activate)
+        {
+            tokenbutton_b.SetActive(false);
+            tokenbutton_s.SetActive(false);
+            tokenbutton_g.SetActive(false);
+        }
     }
 }
---
+++ Assets/StaticScripts/Token.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Token : MonoBehaviour
+{
+    public TestMainMenu menu;
+
+    public Vector3 origin;
+
+    void Awake()
+    {
+        menu = FindObjectOfType<TestMainMenu>();
+        origin = transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        menu.currentToken = gameObject;
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -5);
+    }
+
+    void OnMouseUp()
+    {
+        transform.position = origin;
+        menu.currentToken = null;
+    }
+}
